2b8f7daaaccedea774c98b530b753283c43e51a8
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-06-04 00:44:36 +0530

    forgot password added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
 //@desc     Get single bootcamps
 //@routes   GET /api/v1/bootcamps/:id
-//@access    Public
+//@access   Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id).populate('courses')
 
@@ -30,6 +30,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@routes   POST /api/v1/bootcamps/
 //@access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  //Add user to req.body
+  req.body.user = req.user.id
+
+  //Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id })
+
+  //If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    )
+  }
+
   const bootcamp = await Bootcamp.create(req.body)
   res.status(200).json({ success: true, data: bootcamp })
 })
@@ -38,16 +54,28 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 //@routes   PUT /api/v1/bootcamps/:id
 //@access   Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  })
+  let bootcamp = await Bootcamp.findById(req.params.id)
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     )
   }
+
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    )
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
 
   res.status(200).json({ success: true, data: bootcamp })
 })
@@ -61,6 +89,16 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    )
+  }
+
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
     )
   }
 
@@ -108,6 +146,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     )
   }
 
+   //Make sure user is bootcamp owner
+   if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    )
+  }
+
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 404))
   }
@@ -123,7 +171,7 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   if (!file.size > process.env.MAX_FILE_UPLOAD) {
     return next(
       new ErrorResponse(
-        `Please upload an image leass than {process.env.MAX_FILE_UPLOAD}`,
+        `Please upload an image less than {process.env.MAX_FILE_UPLOAD}`,
         400
       )
     )
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
 const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -19,8 +20,13 @@ const auth = require('./routes/auth')
 
 const app = express()
 
+
+//Body parser
 app.use(express.json())
 // app.use(express.urlencoded({extended : true}))
+
+//Cookie parser
+app.use(cookieParser())
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
